e901556a65007808ad115991620646f8dbc8fe0d
lostie21yo <leon.coxsw@gmail.com>
2022-05-21 18:10:49 +0400

    1

--- yolox/utils/visualize.py
+++ yolox/utils/visualize.py
@@ -62,22 +62,26 @@ def plot_tracking(image, tlwhs, obj_ids, scores=None, frame_id=0, fps=0., ids2=N
     text_thickness = 2
     line_thickness = 3
 
-    radius = max(5, int(im_w/140.))
-    cv2.putText(im, 'frame: %d fps: %.2f num: %d' % (frame_id, fps, len(tlwhs)),
-                (0, int(15 * text_scale)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), thickness=2)
-
     for i, tlwh in enumerate(tlwhs):
         x1, y1, w, h = tlwh
         intbox = tuple(map(int, (x1, y1, x1 + w, y1 + h)))
+        dot = tuple(map(int, (x1+w/2, y1+h/2)))
         obj_id = int(obj_ids[i])
         id_text = '{}'.format(int(obj_id))
         if ids2 is not None:
             id_text = id_text + ', {}'.format(int(ids2[i]))
         color = get_color(abs(obj_id))
-        cv2.circle(im, (x1+w/2, y1+h/2), 5, color=color, thickness=-1)
+        cv2.circle(im, dot, 5, color=color, thickness=-1)
         cv2.rectangle(im, intbox[0:2], intbox[2:4], color=color, thickness=line_thickness)
-        cv2.putText(im, id_text, (intbox[0], intbox[1]), cv2.FONT_HERSHEY_PLAIN, text_scale, (0, 0, 255),
+        cv2.putText(im, id_text, (intbox[0], intbox[1]), cv2.FONT_HERSHEY_PLAIN, text_scale, (255, 255, 255),
                     thickness=text_thickness)
+        if dot[1]<im_h*0.6:
+            pass
+
+    radius = max(5, int(im_w/140.))
+    cv2.putText(im, f'frame: {frame_id} fps: {round(fps, 2)} num: {tlwhs}',
+                (0, int(20 * text_scale)), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), thickness=3)
+
     return im
 
 
